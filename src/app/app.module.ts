5f0fea7817ee4492810e77cf1845b1431bd121c9
Hinder Adrian Alvarez Perlaza <halvarezadrian@gmail.com>
2023-09-24 20:15:34 -0500

    Adding components according mockups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
+import { LoginUserComponent } from './login-user/login-user.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageProductsComponent,
+    LoginUserComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule
